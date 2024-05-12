6d8846fcd3be2ed218694080ecc9d31b461ab274
Artem Suhov <43279115+ArtemSuhov@users.noreply.github.com>
2024-05-12 18:42:07 +0300

    admin file update
    Упрощено выполнение запросов к базе данных за счет прямого вызова метода sql_do экземпляра базы данных.
    Использование распаковки кортежей для упрощения передачи аргументов в SQL-запросы.
    Рефакторинг главного цикла с целью использования структуры if-elif-else для обработки команд.
    Исправлена логика разбора команд для обработки пустых входов и корректного разделения команд и аргументов.

--- backend/admin.py
+++ backend/admin.py
@@ -1,12 +1,8 @@
 from kinopoisk_unofficial.kinopoisk_api_client import KinopoiskApiClient
 from kinopoisk_unofficial.request.films.film_request import FilmRequest
 from init import db
-from db_classes import Film
-from db_classes import Genre
-from db_classes import Country
-from db_classes import Actor
+from db_classes import Film, Genre, Country, Actor
 from kinopoisk_unofficial.request.staff.person_request import PersonRequest
-import sys
 
 api_client = KinopoiskApiClient("7a0fcfa4-0ad1-43d2-a148-e3dcb778d11c")
 
@@ -15,217 +11,146 @@ def add_film_kp(id_kp: int):
     request = FilmRequest(id_kp)
     response = api_client.films.send_film_request(request)
 
-
-    query = "INSERT INTO Films (name, description, budget, rating, dateFound) VALUES ( ?, ?, ?, ?, ?)"
     film = response.film
 
-    db.sql_do(query, [str(film.name_ru),
-                      str(film.description),
-                      1,
-                      float(film.rating_kinopoisk),
-                      film.year])
+    query = "INSERT INTO Films (name, description, budget, rating, dateFound) VALUES (?, ?, ?, ?, ?)"
+    db.sql_do(query, [film.name_ru, film.description, 1, float(film.rating_kinopoisk), film.year])
 
     film_id = Film.get_film_count()
 
     for genre in film.genres:
-        print(genre.genre)
         genre_bd = Genre.get_by_name(genre.genre)
-        genre_id = None
-
         if genre_bd:
             genre_id = genre_bd.id
-
-        if genre_id:
-            query = "INSERT INTO FilmsGenres VALUES (?, ?)"
-
-            db.sql_do(query, [film_id, genre_id])
         else:
-            query = "INSERT INTO Genres (name) VALUES (?)"
-            db.sql_do(query, [genre.genre])
-
+            db.sql_do("INSERT INTO Genres (name) VALUES (?)", [genre.genre])
             genre_bd = Genre.get_by_name(genre.genre)
+            genre_id = genre_bd.id
 
-            query = "INSERT INTO FilmsGenres VALUES (?, ?)"
-
-            db.sql_do(query, [film_id, genre_bd.id])
+        db.sql_do("INSERT INTO FilmsGenres VALUES (?, ?)", [film_id, genre_id])
 
     for country in film.countries:
-        print(country.country)
         country_bd = Country.get_by_name(country.country)
-        country_id = None
-
         if country_bd:
             country_id = country_bd.id
-
-        if country_id:
-            query = "INSERT INTO FilmsCountries VALUES (?, ?)"
-
-            db.sql_do(query, [film_id, country_id])
         else:
-            query = "INSERT INTO Countries (name) VALUES (?)"
-
-            db.sql_do(query, [country.country])
-
+            db.sql_do("INSERT INTO Countries (name) VALUES (?)", [country.country])
             country_bd = Country.get_by_name(country.country)
+            country_id = country_bd.id
 
-            query = "INSERT INTO FilmsCountries VALUES (?, ?)"
-
-            db.sql_do(query, [film_id, country_bd.id])
+        db.sql_do("INSERT INTO FilmsCountries VALUES (?, ?)", [film_id, country_id])
 
 
 def add_actor_kp(id_kp: int):
     request = PersonRequest(id_kp)
     response = api_client.staff.send_person_request(request)
 
-    query = "INSERT INTO Actors (surname, name, wikiLink, genderId, dateBirth) VALUES ( ?, ?, ?, ?, ?)"
     actor = response
+    gender_id = 1 if actor.sex.name == actor.sex.FEMALE else 2
 
-    gender_id = 1
-    if actor.sex.name == actor.sex.FEMALE:
-        gender_id = 2
-
-    db.sql_do(query,
-              [actor.nameRu.split(" ")[1],
-               actor.nameRu.split(" ")[0],
-               "https://ru.wikipedia.org/",
-               gender_id,
-               actor.birthday])
+    db.sql_do("INSERT INTO Actors (surname, name, wikiLink, genderId, dateBirth) VALUES (?, ?, ?, ?, ?)",
+              [actor.nameRu.split(" ")[1], actor.nameRu.split(" ")[0], "https://ru.wikipedia.org/",
+               gender_id, actor.birthday])
 
     actor_id = Actor.get_actors_count()
 
     for film in actor.films:
-        print(film.name_ru)
         film_bd = Film.get_by_name(film.name_ru)
-        film_id = None
-
         if film_bd:
             film_id = film_bd.id
-
-        if film_id:
-            query = "INSERT INTO FilmsActors VALUES (?, ?)"
-
-            db.sql_do(query, [film_id, actor_id])
+            db.sql_do("INSERT INTO FilmsActors VALUES (?, ?)", [film_id, actor_id])
 
     country_bd = Country.get_by_name(actor.birthplace)
-    country_id = None
-
     if country_bd:
         country_id = country_bd.id
-
-    if country_id:
-        query = "INSERT INTO ActorsCountries VALUES (?, ?)"
-
-        db.sql_do(query, [actor_id, country_id])
     else:
-        query = "INSERT INTO Countries (name) VALUES (?)"
-
-        db.sql_do(query, [actor.birthplace])
-
+        db.sql_do("INSERT INTO Countries (name) VALUES (?)", [actor.birthplace])
         country_bd = Country.get_by_name(actor.birthplace)
+        country_id = country_bd.id
 
-        query = "INSERT INTO ActorsCountries VALUES (?, ?)"
-
-        db.sql_do(query, [actor_id, country_bd.id])
+    db.sql_do("INSERT INTO ActorsCountries VALUES (?, ?)", [actor_id, country_id])
 
 
 def del_film(id: int):
-    query = "DELETE from Films where id = ?"
-    db.sql_do(query, [id])
-
-    query = "DELETE from FilmsCountries where idFilm = ?"
-    db.sql_do(query, [id])
-
-    query = "DELETE from FilmsGenres where idFilm = ?"
-    db.sql_do(query, [id])
-
-    query = "DELETE from FilmsActors where idFilm = ?"
-    db.sql_do(query, [id])
-
-    query = "DELETE from FilmsStudios where idFilm = ?"
-    db.sql_do(query, [id])
+    db.sql_do("DELETE FROM Films WHERE id = ?", [id])
+    db.sql_do("DELETE FROM FilmsCountries WHERE idFilm = ?", [id])
+    db.sql_do("DELETE FROM FilmsGenres WHERE idFilm = ?", [id])
+    db.sql_do("DELETE FROM FilmsActors WHERE idFilm = ?", [id])
+    db.sql_do("DELETE FROM FilmsStudios WHERE idFilm = ?", [id])
 
 
 def del_actor(id: int):
-    query = "DELETE from Actors where id = ?"
-    db.sql_do(query, [id])
-
-    query = "DELETE from ActorsCountries where idActor = ?"
-    db.sql_do(query, [id])
-
-    query = "DELETE from FilmsActors where idActor = ?"
-    db.sql_do(query, [id])
+    db.sql_do("DELETE FROM Actors WHERE id = ?", [id])
+    db.sql_do("DELETE FROM ActorsCountries WHERE idActor = ?", [id])
+    db.sql_do("DELETE FROM FilmsActors WHERE idActor = ?", [id])
 
 
 def add_film(name, description, budget, rating, dateFound):
-    query = "INSERT INTO Films (name, description, budget, rating, dateFound) VALUES ( ?, ?, ?, ?, ?)"
-    db.sql_do(query, [name, description, budget, rating, dateFound])
+    db.sql_do("INSERT INTO Films (name, description, budget, rating, dateFound) VALUES (?, ?, ?, ?, ?)",
+              [name, description, budget, rating, dateFound])
 
 
 def add_actor(id, surname, name, wikiLink, genderId, dateBirth):
-    query = "INSERT INTO Actors (surname, name, wikiLink, genderId, dateBirth) VALUES ( ?, ?, ?, ?, ?)"
-    db.sql_do(query, [surname, name, wikiLink, genderId, dateBirth])
+    db.sql_do("INSERT INTO Actors (surname, name, wikiLink, genderId, dateBirth) VALUES (?, ?, ?, ?, ?)",
+              [surname, name, wikiLink, genderId, dateBirth])
 
 
 def edit_film(id, name=None, description=None, budget=None, rating=None, dateFound=None):
     if name:
-        query = "Update Films set name = ? where id = ?"
-        db.sql_do(query, [name, id])
+        db.sql_do("UPDATE Films SET name = ? WHERE id = ?", [name, id])
     if description:
-        query = "Update Films set description = ? where id = ?"
-        db.sql_do(query, [description, id])
+        db.sql_do("UPDATE Films SET description = ? WHERE id = ?", [description, id])
     if budget:
-        query = "Update Films set budget = ? where id = ?"
-        db.sql_do(query, [budget, id])
+        db.sql_do("UPDATE Films SET budget = ? WHERE id = ?", [budget, id])
     if rating:
-        query = "Update Films set rating = ? where id = ?"
-        db.sql_do(query, [rating, id])
+        db.sql_do("UPDATE Films SET rating = ? WHERE id = ?", [rating, id])
     if dateFound:
-        query = "Update Films set dateFound = ? where id = ?"
-        db.sql_do(query, [dateFound, id])
+        db.sql_do("UPDATE Films SET dateFound = ? WHERE id = ?", [dateFound, id])
 
 
 def edit_actor(id, surname=None, name=None, wikiLink=None, genderId=None, dateBirth=None):
     if surname:
-        query = "Update Actors set surname = ? where id = ?"
-        db.sql_do(query, [surname, id])
+        db.sql_do("UPDATE Actors SET surname = ? WHERE id = ?", [surname, id])
     if name:
-        query = "Update Films set name = ? where id = ?"
-        db.sql_do(query, [name, id])
+        db.sql_do("UPDATE Actors SET name = ? WHERE id = ?", [name, id])
     if wikiLink:
-        query = "Update Films set wikiLink = ? where id = ?"
-        db.sql_do(query, [wikiLink, id])
+        db.sql_do("UPDATE Actors SET wikiLink = ? WHERE id = ?", [wikiLink, id])
     if genderId:
-        query = "Update Films set genderId = ? where id = ?"
-        db.sql_do(query, [genderId, id])
+        db.sql_do("UPDATE Actors SET genderId = ? WHERE id = ?", [genderId, id])
     if dateBirth:
-        query = "Update Films set dateBirth = ? where id = ?"
-        db.sql_do(query, [dateBirth, id])
+        db.sql_do("UPDATE Actors SET dateBirth = ? WHERE id = ?", [dateBirth, id])
 
 
 if __name__ == '__main__':
     while True:
         command = input("Write a command: ")
-        match command.split():
-            case ["quit"]:
-                break
-            case ["?"]:
-                print("add_actor ; add_actor_kp; edit_actor; del_actor;")
-                print("add_film ; add_film _kp; edit_film; del_film;")
-            case ["add_actor", id, surname, name, wikiLink, genderId, dateBirth]:
-                add_actor(id, surname, name, wikiLink, genderId, dateBirth)
-            case ["add_film", id, name, description, budget, rating, dateFound]:
-                add_film(id, name, description, budget, rating, dateFound)
-            case ["add_film_kp", id]:
-                add_film_kp(id)
-            case ["add_actor_kp", id]:
-                add_actor_kp(id)
-            case ["del_actor", id]:
-                del_actor(id)
-            case ["del_film", id]:
-                del_film(id)
-            case ["edit_actor", id, surname, name, wikiLink, genderId, dateBirth]:
-                edit_actor(id, surname, name, wikiLink, genderId, dateBirth)
-            case ["edit_film", id, name, description, budget, rating, dateFound]:
-                edit_actor(id, name, description, budget, rating, dateFound)
-            case _:
-                print(f"Sorry, I couldn't understand {command!r}")
+        parts = command.split()
+        if not parts:
+            continue
+
+        cmd = parts[0]
+        args = parts[1:]
+
+        if cmd == "quit":
+            break
+        elif cmd == "?":
+            print("add_actor, add_actor_kp, edit_actor, del_actor")
+            print("add_film, add_film_kp, edit_film, del_film")
+        elif cmd == "add_actor":
+            add_actor(*args)
+        elif cmd == "add_film":
+            add_film(*args)
+        elif cmd == "add_film_kp":
+            add_film_kp(*args)
+        elif cmd == "add_actor_kp":
+            add_actor_kp(*args)
+        elif cmd == "del_actor":
+            del_actor(*args)
+        elif cmd == "del_film":
+            del_film(*args)
+        elif cmd == "edit_actor":
+            edit_actor(*args)
+        elif cmd == "edit_film":
+            edit_film(*args)
+        else:
+            print(f"Sorry, I couldn't understand {command!r}")
